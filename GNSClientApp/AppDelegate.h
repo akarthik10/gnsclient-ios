57b953321e929ab1b4a9462227eb4dc9318b8a2e
Karthik A <akarthik101@gmail.com>
2017-02-07 17:10:30 -0500

    Initial Commit

---
+++ GNSClientApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GNSClientApp
+//
+//  Created by Karthik A on 2/7/17.
+//  Copyright © 2017 Karthik A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GNSClientApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GNSClientApp
+//
+//  Created by Karthik A on 2/7/17.
+//  Copyright © 2017 Karthik A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
