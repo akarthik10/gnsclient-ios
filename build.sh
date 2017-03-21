bba6d3d3ef720d608620a613ef5993d21cc6cf57
Karthik A <akarthik101@gmail.com>
2017-03-21 00:55:58 -0400

    Update static url

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-URL=`curl -s https://api.github.com/repos/google/j2objc/releases` # | grep browser_download_url` | head -n 1 | cut -d '"' -f 4`
-echo $URL
+URL="https://github.com/google/j2objc/releases/download/1.3.1/j2objc-1.3.1.zip" #`curl -s https://api.github.com/repos/google/j2objc/releases | grep browser_download_url` | head -n 1 | cut -d '"' -f 4`
 wget $URL
 unzip *.zip -d j2objc
