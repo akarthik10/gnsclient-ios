1de183d20c07644f4c8ec5bc4787df44da1636bf
Karthik A <akarthik101@gmail.com>
2017-03-21 02:31:02 -0400

    Add test

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 URL="https://github.com/google/j2objc/releases/download/1.3.1/j2objc-1.3.1.zip" #`curl -s https://api.github.com/repos/google/j2objc/releases | grep browser_download_url` | head -n 1 | cut -d '"' -f 4`
 wget $URL
 unzip *.zip
-xcodebuild -configuration Debug -target GNSClientApp -arch i386 -sdk iphonesimulator
+codebuild -configuration Debug -target GNSClientApp -scheme GNSClientApp -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test
