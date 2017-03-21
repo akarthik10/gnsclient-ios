fab792d33b1125fcab77883e8fa3fdc089d02a34
Karthik A <akarthik101@gmail.com>
2017-03-21 12:29:47 -0400

    Add GNS

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+cd GNS
+ant
+./bin/gpServer.sh start all
+cd ..
+
 URL="https://github.com/google/j2objc/releases/download/1.3.1/j2objc-1.3.1.zip" #`curl -s https://api.github.com/repos/google/j2objc/releases | grep browser_download_url` | head -n 1 | cut -d '"' -f 4`
 wget $URL
 unzip *.zip
