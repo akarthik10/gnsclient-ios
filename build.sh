597fc93155464dfce7f9e3c6686235ba1ca6d068
Karthik A <akarthik101@gmail.com>
2017-03-21 02:41:56 -0400

    Add scheme

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 URL="https://github.com/google/j2objc/releases/download/1.3.1/j2objc-1.3.1.zip" #`curl -s https://api.github.com/repos/google/j2objc/releases | grep browser_download_url` | head -n 1 | cut -d '"' -f 4`
 wget $URL
 unzip *.zip
+xcodebuild -list
 xcodebuild -configuration Debug -target GNSClientApp -scheme GNSClientApp -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test
