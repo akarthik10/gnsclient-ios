1351b9daf778008e33c17c0c157fc4fd369fbdaf
Karthik A <akarthik101@gmail.com>
2017-03-21 00:51:17 -0400

    Updated

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-URL=`curl -s https://api.github.com/repos/google/j2objc/releases | grep browser_download_url` # | head -n 1 | cut -d '"' -f 4`
+URL=`curl -s https://api.github.com/repos/google/j2objc/releases` # | grep browser_download_url` | head -n 1 | cut -d '"' -f 4`
 echo $URL
 wget $URL
 unzip *.zip -d j2objc
